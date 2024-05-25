55e3feb8ba02905f80a98d996abcf8aef9f3870a
Leticia Pimenta <13leticia.pimentas@gmail.com>
2024-05-25 10:48:40 -0300

    add login and home routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'home', component: HomeComponent}
+];
