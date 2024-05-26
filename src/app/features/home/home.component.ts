63f8f890b0189477f4dff5b1886b89a45581b23e
Leticia Pimenta <13leticia.pimentas@gmail.com>
2024-05-26 17:15:17 -0300

    created card with the highest and lowest price

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,27 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from '../../core/models/app-state.model';
 import { selectCryptoLoading, selectCryptoPrices } from '../../store/selectors/crypto.selectors';
 import { loadCryptoPrices } from '../../store/actions/crypto.actions';
 import { CommonModule } from '@angular/common';
+import { PriceSummary } from '../../core/models/price-sumary.model';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit{
   title = 'cryptotracker';
   prices$: Observable<{ [ symbol: string ] : number }>;
   loading$: Observable<boolean>;
   symbols = ['bitcoin', 'ethereum'];
+  priceSummary: PriceSummary[] = [];
 
   constructor(private store: Store<AppState>) {
     this.prices$ = this.store.select(selectCryptoPrices);
     this.loading$ = this.store.select(selectCryptoLoading);
-    this.store.dispatch(loadCryptoPrices());
+
   }
+
+  ngOnInit() {
+    this.store.dispatch(loadCryptoPrices());
+
+    this.prices$.subscribe(prices => {
+      this.getHighestPrice(prices);
+    })
+  }
+
+  getHighestPrice(prices: { [ symbol: string ] : number } ) {
+    for (const symbol of this.symbols) {
+      const price  = prices[symbol];
+      if (price === undefined || isNaN(price)) {
+        continue;
+      }
+      let priceSummaryEntry = this.priceSummary.find(entry => entry.symbol === symbol)
+
+      if (!priceSummaryEntry) {
+        priceSummaryEntry = { symbol, highestPrice: price, lowestPrice: price }
+        this.priceSummary.push(priceSummaryEntry);
+      } else {
+        if (price > priceSummaryEntry.highestPrice ) {
+          priceSummaryEntry.highestPrice = price;
+        }
+        if (price < priceSummaryEntry.lowestPrice) {
+          priceSummaryEntry.lowestPrice = price;
+        }
+      }
+    }
+  }
+
 }
